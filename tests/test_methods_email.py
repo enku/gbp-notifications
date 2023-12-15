f24de9be5d9e2e0c9eecc9bb2a7a9e78982979b4
Albert Hopkins <marduk@letterboxes.org>
2023-12-14 22:08:01 -0600

    tests: add tests for email

--- tests/test_methods_email.py
+++ tests/test_methods_email.py
@@ -4,10 +4,60 @@ from unittest import mock
 
 from gentoo_build_publisher.common import Build, GBPMetadata, Package, PackageMetadata
 
-from gbp_notifications import Event, Recipient
+from gbp_notifications import Event, Recipient, Subscription
 from gbp_notifications.methods import email
+from gbp_notifications.settings import Settings
 
 from . import TestCase
+
+
+@mock.patch.object(email, "Worker")
+class SendTests(TestCase):
+    """Tests for the EmailMethod.send method"""
+
+    event = Event(
+        name="build_pulled",
+        machine="babette",
+        data={"build": Build(machine="babette", build_id="25")},
+    )
+    recipient = Recipient(name="marduk", email="marduk@host.invalid")
+    settings = Settings(
+        RECIPIENTS=(recipient,),
+        SUBSCRIPTIONS={event: Subscription(subscribers=(recipient,))},
+        EMAIL_FROM="gbp@host.invalid",
+    )
+    method = email.EmailMethod(settings)
+    msg = method.compose(event, recipient).as_string()
+
+    def test(self, mock_worker) -> None:
+        self.method.send(self.event, self.recipient)
+
+        mock_worker.return_value.run.assert_called_once()
+        args, kwargs = mock_worker.return_value.run.call_args
+        self.assertEqual(
+            args,
+            (
+                email.sendmail,
+                "gbp@host.invalid",
+                ["marduk <marduk@host.invalid>"],
+                self.msg,
+            ),
+        )
+        self.assertEqual(kwargs, {})
+
+    @mock.patch.object(email, "logger")
+    def test_with_missing_template(self, mock_logger, mock_worker) -> None:
+        event = Event(
+            name="bogus",
+            machine="babette",
+            data={"build": Build(machine="babette", build_id="25")},
+        )
+        self.method.send(event, self.recipient)
+
+        mock_worker.return_value.run.assert_not_called()
+        mock_logger.warning.assert_called_once_with(
+            "No template found for event: %s", "bogus"
+        )
 
 
 class SendmailTests(TestCase):
