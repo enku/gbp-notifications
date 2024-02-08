89532fbce36d031a64df0f837cc5f23d3bedf545
Albert Hopkins <marduk@letterboxes.org>
2024-02-08 17:06:06 -0600

    pathlib.Path can take muliple **args
    TIL

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -17,7 +17,7 @@ class SettingTests(TestCase):
         self.assertEqual(settings.email_password, "foobar")
 
     def test_email_password_from_file(self) -> None:
-        pw_file = Path(self.tmpdir) / "password"
+        pw_file = Path(self.tmpdir, "password")
         pw_file.write_text("foobar", encoding="UTF-8")
 
         settings = Settings(EMAIL_SMTP_PASSWORD_FILE=str(pw_file))
@@ -25,7 +25,7 @@ class SettingTests(TestCase):
         self.assertEqual(settings.email_password, "foobar")
 
     def test_email_password_prefer_file(self) -> None:
-        pw_file = Path(self.tmpdir) / "password"
+        pw_file = Path(self.tmpdir, "password")
         pw_file.write_text("file", encoding="UTF-8")
 
         settings = Settings(
@@ -45,7 +45,7 @@ bob = {email = "bob@host.invalid"}
 [subscriptions]
 babette = {pull = ["marduk", "bob"], foo = ["marduk"]}
 """
-        config_file = Path(self.tmpdir) / "config.toml"
+        config_file = Path(self.tmpdir, "config.toml")
         config_file.write_text(toml, encoding="UTF-8")
         settings = Settings.from_dict("", {"CONFIG_FILE": str(config_file)})
 
