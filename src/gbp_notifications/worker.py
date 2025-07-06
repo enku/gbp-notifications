00d61218bf626e241244b434637ff2d9d4607f9e
Albert Hopkins <marduk@letterboxes.org>
2025-07-06 07:38:04 -0500

    worker.run: add better type annotation

--- src/gbp_notifications/worker.py
+++ src/gbp_notifications/worker.py
@@ -1,12 +1,14 @@
 """Access to the Gentoo Build Publisher Worker"""
 
-from typing import Any, Callable
+from typing import Any, Callable, ParamSpec
 
 from gentoo_build_publisher.settings import Settings
 from gentoo_build_publisher.worker import Worker
 
+P = ParamSpec("P")
 
-def run(func: Callable[..., Any], *args: Any, **kwargs: Any) -> None:
+
+def run(func: Callable[P, Any], *args: P.args, **kwargs: P.kwargs) -> None:
     """Run the given func with args as a GBP worker task"""
     worker = Worker(Settings.from_environ())
     worker.run(func, *args, **kwargs)
