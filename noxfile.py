6b6efb5f88409976ef10f03c6ce774e0d5162574
Albert Hopkins <marduk@letterboxes.org>
2025-08-10 07:28:56 -0500

    add a noxfile
    For CI/CD.

---
+++ noxfile.py
@@ -0,0 +1,22 @@
+"""noxfile for ci/cd testing"""
+
+# pylint: disable=missing-docstring
+import nox
+
+PYTHON_VERSIONS = (
+    "3.11",
+    "3.12",
+    "3.13",
+    # "3.14",  # python-dispatch does not (yet) work with this
+)
+
+
+@nox.session(python=PYTHON_VERSIONS)
+def tests(session: nox.Session) -> None:
+    dev_dependencies = nox.project.load_toml("pyproject.toml")["dependency-groups"][
+        "dev"
+    ]
+    session.install(".", *dev_dependencies)
+
+    session.run("coverage", "run", "-m", "tests")
+    session.run("coverage", "report")
